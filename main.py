4140bfcec4fd177f1f258a6ac1e6a5daac8145ba
TYCheng31 <117744027+TYCheng31@users.noreply.github.com>
2026-09-23 14:04:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 import os
 import time
 import re
